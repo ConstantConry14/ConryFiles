106f4e5ad9c02ad0441a477feae8056a52e918d0
ConstantConry14 <cwilliams1005@gmail.com>
2017-11-26 14:27:58 -0800

    Add files via upload

---
+++ Polymon/src/polymon/Cat.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polymon;
+
+/**
+ *
+ * @author Conry
+ */
+public class Cat extends Animal{
+    
+    
+    public void catSpeaks(){
+    
+        System.out.println("The Cat");
+    }
+    
+}
