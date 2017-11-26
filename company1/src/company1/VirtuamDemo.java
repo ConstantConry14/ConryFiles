0120ec29f0f73420810834eb8348304517b72c80
ConstantConry14 <cwilliams1005@gmail.com>
2017-11-26 14:14:38 -0800

    Add files via upload

---
+++ company1/src/company1/VirtuamDemo.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package company1;
+
+
+public class VirtuamDemo {
+    public static void main(String arg[]){
+    
+      salary s = new salary("Mohd Mohtashim", "Ambehta, UP", 3, 3600.00);
+      employee e = new salary("John Adams", "Boston, MA", 2, 2400.00);
+      salary s1 = new salary("Mohd Mohtashim", "Ambehta, UP", 3, 3600.00);
+      employee e1 = new salary("John Adams", "Boston, MA", 2, 2400.00);
+      salary s2 = new salary("Mohd Mohtashim", "Ambehta, UP", 3, 3600.00);
+      employee e2 = new salary("John Adams", "Boston, MA", 2, 2400.00);
+      salary s3 = new salary("Mohd Mohtashim", "Ambehta, UP", 3, 3600.00);
+      employee e3 = new salary("John Adams", "Boston, MA", 2, 2400.00);
+      System.out.println("");
+    e.checkMail();
+    e1.checkMail();
+    e2.checkMail();
+    e3.checkMail();
+        System.out.println("");
+    s.checkMail();
+    s1.checkMail();
+    s2.checkMail();
+    s3.checkMail();
+    }
+    
+}
